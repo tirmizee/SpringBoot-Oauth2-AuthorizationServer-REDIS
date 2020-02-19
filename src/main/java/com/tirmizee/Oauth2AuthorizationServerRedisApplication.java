9442cc3c5be4611e00e495ad445d93a63008a927
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-02-20 00:33:09 +0700

    init project

---
+++ src/main/java/com/tirmizee/Oauth2AuthorizationServerRedisApplication.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2AuthorizationServerRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2AuthorizationServerRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tirmizee/Oauth2AuthorizationServerRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tirmizee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AuthorizationServerRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
